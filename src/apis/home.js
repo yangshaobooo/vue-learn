7043f9494be65c70dfb1a2d083043077835bacd6
yangshaobooo <1783494374@qq.com>
2024-05-13 16:52:35 +0800

    新鲜好物

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,9 +1,15 @@
 import  httpInstance from '@/utils/http'
 
+// 获取banner 
 export function getBannerAPI(){
     return httpInstance({
         url: '/home/banner'
     })
 }
 
-// 获取banner 
+// 获取新鲜好物内容
+export const findNewAPI = () => {
+    return httpInstance({
+      url:'/home/new'
+    })
+  }
