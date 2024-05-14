426ab6261c42dab4a348861a11fdbb9ba1003112
yangshaobooo <1783494374@qq.com>
2024-05-14 10:38:32 +0800

    二级分类-基础列表渲染

--- src/apis/category.js
+++ src/apis/category.js
@@ -17,3 +17,22 @@ export const getTopCategoryAPI = (id) => {
       }
     })
   }
+
+  // 获取二级分类商品
+  /**
+ * @description: 获取导航数据
+ * @data { 
+     categoryId: 1005000 ,
+     page: 1,
+     pageSize: 20,
+     sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+   } 
+ * @return {*}
+ */
+export const getSubCategoryAPI = (data) => {
+  return request({
+    url:'/category/goods/temporary',
+    method:'POST',
+    data
+  })
+}
