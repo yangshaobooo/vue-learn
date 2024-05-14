9dbb8f2fa7e0748fa5428e2b0927388302e1d655
yangshaobooo <1783494374@qq.com>
2024-05-14 11:49:29 +0800

    二级分类路由滑动优化

--- src/router/index.js
+++ src/router/index.js
@@ -32,7 +32,13 @@ const router = createRouter({
       path: '/login',
       component: Login
     }
-  ]
+  ],
+  // 路由滚动行为配置项
+  scrollBehavior(){
+    return {
+      top:0
+    }
+  }
 })
 
 export default router
